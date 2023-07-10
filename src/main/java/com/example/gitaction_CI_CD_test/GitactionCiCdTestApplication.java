faccb4174dd0ae991ed9a9cb3355075a61efba7a
BonSik-Koo <rnqhstlr2297@gmail.com>
2023-07-10 23:46:32 +0900

    init: 초기 프로젝트 세팅

---
+++ src/main/java/com/example/gitaction_CI_CD_test/GitactionCiCdTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitaction_CI_CD_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitactionCiCdTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitactionCiCdTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitaction_CI_CD_test/GitactionCiCdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitaction_CI_CD_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitactionCiCdTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
